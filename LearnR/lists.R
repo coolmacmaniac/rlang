5dfbc92bcb7dbb3937ea09610f8af329fd22ef43
Hans Kammler <coolmacmaniac@gmail.com>
2019-01-19 20:32:59 +0530

    Lists about lists

---
+++ LearnR/lists.R
@@ -0,0 +1,7 @@
+# with elements of different class types
+x <- list(3.14, 108L, "s", 'xxx', TRUE, F, 6+9i)
+x
+
+# list is a vector but it allows to store different typed elements
+y <- vector(mode = "list", length = 5)
+y
