df4491afd7c319dd31e74568b43af6de53cbff0a
Hans Kammler <coolmacmaniac@gmail.com>
2019-02-02 22:41:15 +0530

    facets exploring facets for categorical variables

--- dsci/visualize/Plotting.R
+++ dsci/visualize/Plotting.R
@@ -16,14 +16,14 @@ glimpse(mtcars)
 plt <- ggplot(data = mpg)
 
 # define the x-y mapping aesthetics
-# viz_lyr <- aes(x = displ, y = hwy)
+viz_lyr <- aes(x = displ, y = hwy)
 # viz_lyr <- aes(x = hwy, y = cyl)
 # viz_lyr <- aes(x = class, y = drv)
 
 # viz_lyr <- aes(x = displ, y = hwy, colour = class)    # unordered = unordered
 # viz_lyr <- aes(x = displ, y = hwy, size = class)      # ordered = unordered
 # viz_lyr <- aes(x = displ, y = hwy, alpha = class)     # ordered = unordered
-# viz_lyr <- aes(x = displ, y = hwy, shape = class)       # unordered = unordered
+# viz_lyr <- aes(x = displ, y = hwy, shape = class)     # unordered = unordered
 
 # viz_lyr <- aes(x = displ, y = hwy, colour = cty)      # scaled = ordered
 # viz_lyr <- aes(x = displ, y = hwy, size = cty)        # scaled = ordered
@@ -40,14 +40,33 @@ plt <- ggplot(data = mpg)
 # viz_lyr <- aes(x = displ, y = hwy, alpha = cty, size = year)
 # viz_lyr <- aes(x = displ, y = hwy, alpha = year, size = cty)
 
-viz_lyr <- aes(x = displ, y = hwy, color = displ < 5 | hwy <= 20)
+# viz_lyr <- aes(x = displ, y = hwy, color = displ < 5 | hwy <= 20)
 # viz_lyr <- aes(x = displ, y = hwy, color = displ > 5 & hwy > 20)
 
 # do the plotting
-plt + geom_point(mapping = viz_lyr)
+# plt + geom_point(mapping = viz_lyr)
 # plt + geom_point(mapping = viz_lyr, color = "#1068B5")
 # plt + geom_point(
 #   mapping = viz_lyr,
 #   shape = 23, color = 'blue', fill = 'green', size = 2, stroke = 1
 #   )
 # plt + geom_count(mapping = viz_lyr)
+
+count(mpg, drv)
+count(mpg, cyl)
+count(mpg, drv, cyl)
+
+# facets for categorical variables
+# one variable at a time
+plt + geom_point(mapping = viz_lyr) + facet_wrap(~ class, nrow = 1)
+plt + geom_point(mapping = viz_lyr) + facet_wrap(~ class, ncol = 1)
+plt + geom_point(mapping = viz_lyr) + facet_wrap(~ class, nrow = 2)
+plt + geom_point(mapping = viz_lyr) + facet_wrap(~ class)
+plt + geom_point(mapping = viz_lyr) + facet_wrap(drv ~ cyl, labeller = labeller(drv = label_both, cyl = label_both))
+# two variables at a time
+plt + geom_point(mapping = viz_lyr) + facet_grid(. ~ class)
+plt + geom_point(mapping = viz_lyr) + facet_grid(class ~ .)
+plt + geom_point(mapping = viz_lyr) + facet_grid(drv ~ cyl, labeller = labeller(drv = label_both, cyl = label_both))
+plt + geom_point(mapping = viz_lyr) + facet_grid(. ~ cyl)
+plt + geom_point(mapping = viz_lyr) + facet_grid(drv ~ .)
+
