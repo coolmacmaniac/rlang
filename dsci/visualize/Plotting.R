d601dfc356ba75339c736930ab9b1437b16ce92a
Hans Kammler <coolmacmaniac@gmail.com>
2019-02-10 19:17:52 +0530

    geom exploring geometric objects

--- dsci/visualize/Plotting.R
+++ dsci/visualize/Plotting.R
@@ -16,7 +16,7 @@ glimpse(mtcars)
 plt <- ggplot(data = mpg)
 
 # define the x-y mapping aesthetics
-viz_lyr <- aes(x = displ, y = hwy)
+# viz_lyr <- aes(x = displ, y = hwy)
 # viz_lyr <- aes(x = hwy, y = cyl)
 # viz_lyr <- aes(x = class, y = drv)
 
@@ -69,4 +69,106 @@ plt + geom_point(mapping = viz_lyr) + facet_grid(class ~ .)
 plt + geom_point(mapping = viz_lyr) + facet_grid(drv ~ cyl, labeller = labeller(drv = label_both, cyl = label_both))
 plt + geom_point(mapping = viz_lyr) + facet_grid(. ~ cyl)
 plt + geom_point(mapping = viz_lyr) + facet_grid(drv ~ .)
+
+
+# geometric objects
+viz_lyr <- aes(x = displ, y = hwy)
+viz_lyr <- aes(x = displ, y = hwy, linetype = drv)
+plt + geom_smooth(mapping = viz_lyr)
+
+plt +
+  geom_smooth(mapping = aes(x = displ, y = hwy, linetype = drv, color = drv)) +
+  geom_point(mapping = aes(x = displ, y = hwy, color = drv))
+
+plt +
+  geom_smooth(mapping = aes(x = displ, y = hwy))
+
+plt +
+  geom_smooth(mapping = aes(x = displ, y = hwy, group = drv))
+
+plt +
+  geom_smooth(
+    mapping = aes(x = displ, y = hwy, colour = drv),
+    show.legend = F
+    )
+
+plt +
+  geom_smooth(mapping = aes(x = displ, y = hwy, color = drv)) +
+  geom_point(mapping = aes(x = displ, y = hwy, color = drv))
+
+ggplot(
+  data = mpg,
+  mapping = aes(x = displ, y = hwy, color = drv)
+  ) +
+  geom_smooth(show.legend = F) +
+  geom_point(show.legend = F)
+
+ggplot(
+  data = mpg,
+  mapping = aes(x = displ, y = hwy),
+  show.legend = F
+  ) +
+  geom_smooth(show.legend = F) +
+  geom_point(mapping = aes(colour = class), show.legend = F)
+
+ggplot(
+  data = mpg,
+  mapping = aes(x = displ, y = hwy)
+  ) +
+  geom_smooth(
+    data = filter(mpg, class == 'subcompact'),
+    se = FALSE
+  ) +
+  geom_point(mapping = aes(colour = class))
+
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) +
+  # geom_boxplot()
+  # geom_histogram(stat = 'identity')
+  # geom_line()
+  geom_smooth(se = FALSE)
+  # geom_area()
+  # geom_bar(stat = 'identity')
+
+ggplot(
+  data = mpg,
+  mapping = aes(x = displ, y = hwy, color = drv)
+  ) +
+  geom_point() +
+  geom_smooth(se = FALSE)
+
+####
+
+ggplot(mpg, mapping = aes(x = displ, y = hwy)) +
+  geom_point() +
+  geom_smooth(se = F)
+
+ggplot(mpg, mapping = aes(x = displ, y = hwy)) +
+  geom_point() +
+  geom_smooth(mapping = aes(group = drv), se = F)
+
+ggplot(mpg, mapping = aes(x = displ, y = hwy, color = drv)) +
+  geom_point() +
+  geom_smooth(se = F)
+
+ggplot(mpg, mapping = aes(x = displ, y = hwy)) +
+  geom_point(mapping = aes(color = drv)) +
+  geom_smooth(se = F)
+
+ggplot(mpg, mapping = aes(x = displ, y = hwy)) +
+  geom_point(mapping = aes(color = drv)) +
+  geom_smooth(mapping = aes(linetype = drv), se = F)
+
+ggplot(mpg, mapping = aes(x = displ, y = hwy)) +
+  geom_point(
+    shape = 21,
+    size = 5,
+    stroke = 4,
+    color = "white"
+  ) +
+  geom_point(
+    mapping = aes(color = drv),
+    size = 4
+  )
+
+####
 
